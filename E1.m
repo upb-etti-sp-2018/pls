054ac39a0cbfbca464f78349164afdabe2401f05
bogdanbis <44033388+bogdanbis@users.noreply.github.com>
2018-10-20 20:57:59 +0300

    .

---
+++ E1.m
@@ -0,0 +1,11 @@
+%E1.Ini?ializa?i în MATLAB cei doi vectori ?i realiza?i înmul?irea.
+%Ce opera?ie se efectueaz??
+
+a=[1,2,3,4,5];
+b=[5;4;3;2;1];
+%a)
+    a*b
+%b)
+    b*a
+%c)
+    a.*b
---
+++ E3.m
@@ -0,0 +1,13 @@
+%________________________________________________________
+%E3.Realiza?i un program MATLAB care generaz? un vector cu elemente complexe.
+%Realiza?i (un alt fi?ier) o func?ie MATLAB care având drept parametru de
+%intrare vectorul cu valori complexe returneaz? ca parametri de ie?ire:
+%- media aritmetic? a p?r?ilor reale ale elementelor vectorului;
+%- un vector ce con?ine elementele vectorului ini?ial ridicate la p?trat;
+%- o matrice ob?inut? din înmul?irea vectorului ini?ial cu transpusul s?u.
+    
+
+    real=randi([-100 100], 1, 5);
+    im=randi([-100 100], 1, 5)*i;
+    z=real+im
+    
