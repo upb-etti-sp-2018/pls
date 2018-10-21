8941f5778ae8ec056897cc00c153dca241db52fa
bogdanbis <44033388+bogdanbis@users.noreply.github.com>
2018-10-21 13:47:28 +0300

    Delete E51.m

--- E51.m
+++
@@ -1,8 +0,0 @@
-F = 50;
-t = 0:0.0002:0.2;
-s = 2*sin(2*pi*F*t);
-plot(t,s,'.-'),xlabel('Timp [s]'),grid
-
-%schimbarea pasului de la .001 la .0002 face graficul mai precis prin
-%adaugarea mai multor valori in care s este calculat. din acelasi motiv apar mai
-%multe . pe grafic
